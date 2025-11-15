3f926576fd8dc9d515cc85792286133afbe0d2a9
loneraf71 <161646850+loneraf71@users.noreply.github.com>
2025-11-15 15:52:47 +0500

    Update 4_3.cpp

--- 4_3.cpp
+++ 4_3.cpp
@@ -117,7 +117,6 @@ static void makeTex(int N = 256) {
         for (int i = 0; i < N; i++) {
             float u = i / float(N - 1);
             float v = j / float(N - 1);
-            // Исправление переполнения
             unsigned char R = static_cast<unsigned char>(255.0f * u);
             unsigned char G = static_cast<unsigned char>(255.0f * v);
             unsigned char B = static_cast<unsigned char>(255.0f * (1.0f - u));
@@ -139,7 +138,6 @@ static void drawPatch() {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
-    // Настройка материала
     GLfloat mat_diffuse[] = { 0.7f, 0.7f, 0.7f, 1.0f };
     GLfloat mat_specular[] = { 0.6f, 0.6f, 0.6f, 1.0f };
     GLfloat mat_shininess[] = { 32.0f };
@@ -147,7 +145,6 @@ static void drawPatch() {
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 
-    // Настройка света
     GLfloat light_pos[] = { 5.0f, 5.0f, 5.0f, 1.0f };
     GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
     GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
@@ -164,7 +161,6 @@ static void drawPatch() {
         glDisable(GL_TEXTURE_2D);
     }
 
-    // Рисуем патч как сетку треугольников
     glBegin(GL_TRIANGLES);
     for (int j = 0; j < RES - 1; j++) {
         for (int i = 0; i < RES - 1; i++) {
@@ -173,13 +169,11 @@ static void drawPatch() {
             float v1 = j / float(RES - 1);
             float v2 = (j + 1) / float(RES - 1);
 
-            // Вычисляем 4 угловые точки
             Vec3 p00 = evalP(u1, v1);
             Vec3 p10 = evalP(u2, v1);
             Vec3 p01 = evalP(u1, v2);
             Vec3 p11 = evalP(u2, v2);
 
-            // Вычисляем нормали
             Vec3 pu00 = evalPu(u1, v1);
             Vec3 pv00 = evalPv(u1, v1);
             Vec3 n00 = normalize(crossp(pu00, pv00));
@@ -196,7 +190,6 @@ static void drawPatch() {
             Vec3 pv11 = evalPv(u2, v2);
             Vec3 n11 = normalize(crossp(pu11, pv11));
 
-            // Первый треугольник
             glNormal3f(n00.x, n00.y, n00.z);
             glTexCoord2f(u1, v1); glVertex3f(p00.x, p00.y, p00.z);
 
@@ -206,7 +199,6 @@ static void drawPatch() {
             glNormal3f(n11.x, n11.y, n11.z);
             glTexCoord2f(u2, v2); glVertex3f(p11.x, p11.y, p11.z);
 
-            // Второй треугольник
             glNormal3f(n00.x, n00.y, n00.z);
             glTexCoord2f(u1, v1); glVertex3f(p00.x, p00.y, p00.z);
 
@@ -235,7 +227,6 @@ static void display() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // Позиция камеры
     float cy = camDistVal * sinf(camPitchDeg * static_cast<float>(M_PI) / 180.0f);
     float cx = camDistVal * cosf(camPitchDeg * static_cast<float>(M_PI) / 180.0f) *
         cosf(camYawDeg * static_cast<float>(M_PI) / 180.0f);
@@ -302,4 +293,5 @@ int main(int argc, char** argv) {
 
     glutMainLoop();
     return 0;
+
 }
